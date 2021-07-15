9fa898929ec948d3975fe772c2249ce1391dccc6
Viktoria J <jurcsak.viki@gmail.com>
2021-07-15 22:32:30 +0200

    Update 2-Getting_Ready_for_Physics_Class.py

--- 2-Getting_Ready_for_Physics_Class.py
+++ 2-Getting_Ready_for_Physics_Class.py
@@ -1,32 +1,28 @@
 # Act as a physics teacher providing students with functions that will help them calculate fundamental physical properties.
 
-train_mass = 22680
-train_acceleration = 10
-train_distance = 100
-
-bomb_mass = 1
+train_mass = float(input("Enter the weight of the train in kilogramms: "))
+train_acceleration = float(input("Enter the acceleration of the train in m/h: "))
+train_distance = float(input("Enter the distance in meters: "))
+bomb_mass = float(input("Enter the mass of the bomb in kilogramms: "))
 
 def f_to_c(f_temp):
     c_temp = (f_temp - 32) * 5 / 9
     return c_temp
 
 f100_in_celsius = f_to_c(100)
-print(f100_in_celsius)
 
 def c_to_f(c_temp):
     f_temp = c_temp * (9 / 5) + 32
     return f_temp
 
 c0_in_fahrenheit = c_to_f(0)
-print(c0_in_fahrenheit)
 
 def get_force(mass, acceleration):
     return mass * acceleration
 
 train_force = get_force(train_mass, train_acceleration)
-print(train_force)
 
-print("The GE train supplies " + str(train_force) + " Newtons of force.")
+print("\nThe GE train supplies " + str(train_force) + " Newtons of force.")
 
 c = 3 * 10 ** 8
 def get_energy(mass, c):
